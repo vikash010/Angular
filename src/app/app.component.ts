cd516609ceb1c2eba3f12c486d60a38311b71936
vikash <vbhati010@gmail.com>
2021-01-15 11:59:17 +0530

    Word & Countries generator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import arrayWords, { country } from '../utils/words';
+//
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-gen';
+  words = '';
+  countries = '';
+  limit = 10;
+
+  handelSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  generateRandomCountry() {
+    let rndNumber: number = this.getRandomNumber(5);
+
+    this.countries = country.slice(0, rndNumber).join(' ');
+  }
+
+  getRandomNumber(max: number) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+  }
 }
